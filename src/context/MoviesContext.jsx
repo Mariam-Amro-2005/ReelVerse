7b95659d954f848c3f9b6d547016b9d1b3d8e02f
Mariam Amro <mariam.seifeldin.2005@gmail.com>
2025-07-21 06:42:43 +0300

    Added genre selection, media list, and seacrh fn

--- src/context/MoviesContext.jsx
+++ src/context/MoviesContext.jsx
@@ -14,10 +14,21 @@ export const MediaProvider = ({ children }) => {
     const [selectedMediaType, setSelectedMediaType] = useState('movie');
     const [loading, setLoading] = useState(true);
 
+    const [selectedMedia, setSelectedMedia] = useState(null);   // selected movie or TV show
+    // const [selectedMediaId, setSelectedMediaId] = useState(null); // ID of the selected movie or TV show
+    // const [selectedMediaDetails, setSelectedMediaDetails] = useState(null); // Details of the selected movie or TV show
+    const [selectedGenre, setSelectedGenre] = useState(null); // Selected genre
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentMediaList, setCurrentMediaList] = useState([]); // List of movies or TV shows for the current page
+    const [totalResults, setTotalResults] = useState(0);
+    const [totalPages, setTotalPages] = useState(1);
+
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
     const toggleMediaType = () => {
         setSelectedMediaType(prevType => (prevType === 'movie' ? 'tv' : 'movie'));
     };
-
 
     const fetchTrendingContent = async () => {
         setLoading(true);
@@ -56,21 +67,118 @@ export const MediaProvider = ({ children }) => {
             console.error("Failed to fetch genres:", error);
         }
     };
+    
+    const fetchMediaByGenre = async () => {
+        const genreId = selectedGenre?.id;
+        if (!genreId) return;
+        setLoading(true);
+        try {
+            const response = await fetch(`${apiUrl}/discover/${selectedMediaType}?api_key=${apikey}&include_adult=false&include_video=false&language=en-US&page=${currentPage}&sort_by=popularity.desc&with_genres=${genreId}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setTotalPages(data.total_pages);
+            setTotalResults(data.total_results);
+            setCurrentMediaList(data.results || []);
+        } catch (error) {
+            console.error("Failed to fetch media by genre:", error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    // Set search query
+    const setSearchQueryHandler = (query) => {
+        setSearchQuery(query);
+    }
+
+    // Search media by query
+    const searchMedia = async () => {
+        query = searchQuery.trim();
+        if (!query) {
+            setSearchResults([]);
+            return;
+        }
+        setLoading(true); // Need to implement skeleton loading
+        // Fetch search results from TMDB API
+        try {
+            const response = await fetch(`${apiUrl}/search/${selectedMediaType}?api_key=${apikey}&query=${encodeURIComponent(query)}&include_adult=false`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setSearchResults(data.results || []);
+        } catch (error) {
+            console.error("Failed to search media:", error);
+        } finally {
+            setLoading(false);
+        }
+
+        // Setting search results to current media list
+        setCurrentMediaList(searchResults);
+    };
+
+    const filterMedia = () => {
+        // use discover method from TMDB API to filter media by genre, release date, etc.
+        // Receive object with filters and apply them to the current media list
+        // This function will be implemented later
+    }
+
+    // Change page for pagination + or -
+    const changePage = (direction) => {
+        if (direction === 'next' && currentPage < totalPages) {
+            setCurrentPage(prevPage => prevPage + 1);
+        } else if (direction === 'prev' && currentPage > 1) {
+            setCurrentPage(prevPage => prevPage - 1);
+        }
+    }
 
     // Fetch trending content and genres on initial load
     useEffect(() => {
         Promise.all([fetchTrendingContent(), fetchGenres()]);
     }, []);
     
+    // Fetch trending content and genres when media type changes
     useEffect(() => {
         if ((selectedMediaType === 'movie' && !movieGenres.length) || (selectedMediaType === 'tv' && !tvGenres.length)) {
             Promise.all([fetchTrendingContent(), fetchGenres()]);
         }
     }, [selectedMediaType]);
 
+    // Fetch media by genre when selected genre changes
+    useEffect(() => {
+        if (selectedGenre) {
+            fetchMediaByGenre();
+        }
+    }, [selectedGenre]);
+
+    // Fetch search results when search query changes
+    useEffect(() => {
+        if (searchQuery) {
+            searchMedia();
+        }
+        else {
+            setSearchResults([]);
+            setCurrentMediaList([]);
+        }
+    }, [searchQuery]);
+
     return (
-        <MediaContext.Provider value={{ trendingMovies, movieGenres, trendingTvShows, tvGenres, selectedMediaType, loading }}>
+        <MediaContext.Provider value={{ toggleMediaType, trendingMovies, movieGenres, trendingTvShows, tvGenres, selectedMediaType, loading, currentMediaList, totalResults, totalPages, selectedGenre, changePage, setSearchQueryHandler }}>
             {children}
         </MediaContext.Provider>
     );
 }
+
+
+// To do list:
+// 1. implement useEffect for when media list changes
+// 3. Implement media details fetching
+// 4. Implement media selection and details display
+// 5. Implement genre selection functionality
+
+// Finished:
+// 1. Implement search functionality *
+// 2. Implement pagination for media lists
+// 3. Implement trigger for searchMedia fn
